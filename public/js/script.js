9e1bb1ab9f10d9299939f7c25650181dabe5d66f
Quentin ADAM <quentin.adam@waxzce.org>
2012-04-21 20:23:31 +0200

    bugfixe

--- public/js/script.js
+++ public/js/script.js
@@ -32,7 +32,7 @@ var OpenSpice = (function() {
         }).done(function(data) {
             _.each(data,
             function(t, i) {
-                $('#mainmenu').append(this.templates.trackInQueue({
+                $('#mainmenu').append(OpenSpice.templates.trackInQueue({
                     name: t.name,
                     artists: _.pluck(t.artists, 'name').join(', ')
                 }));
