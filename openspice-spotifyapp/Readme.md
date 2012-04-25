2135f7a6e67c8b46599c3bd6a76c3078926a606d
Quentin ADAM <quentin.adam@waxzce.org>
2012-04-25 17:46:47 +0200

    integrate spotify app

---
+++ openspice-spotifyapp/Readme.md
@@ -0,0 +1,8 @@
+WORK IN PROGRESS
+
+installing spotify app :
+https://developer.spotify.com/technologies/apps/guidelines/
+
+js doc 
+https://developer.spotify.com/technologies/apps/docs/beta/
+
---
+++ openspice-spotifyapp/js/script.js
@@ -0,0 +1,46 @@
+var sp = getSpotifyApi(1);
+var models = sp.require('sp://import/scripts/api/models');
+
+
+$(function() {
+    var socket = io.connect('http://colabzik.fake:8066/controll');
+    socket.on('connect',
+    function() {
+		socket.emit('country', models.session.country);
+
+        socket.on('playmusic_order',
+        function(t) {
+            models.player.play(t.href);
+			socket.emit('playing', t);
+        });
+
+
+        socket.on('volume_up',
+        function(msg) {
+            if (models.player.volume < 1) {
+                models.player.volume = models.player.volume + 0.1;
+            }
+        });
+
+        socket.on('volume_down',
+        function(msg) {
+            if (models.player.volume > 0) {
+                models.player.volume = models.player.volume - 0.1;
+            }
+        });
+
+        models.player.observe(models.EVENT.CHANGE,
+        function(e) {
+            //         console.log(e);
+            if (e.type == 'playerStateChanged' && e.data.curtrack) {
+                //			socket.emit('nextmusic_ask', {});
+                }
+        });
+
+
+
+    });
+
+
+
+});
