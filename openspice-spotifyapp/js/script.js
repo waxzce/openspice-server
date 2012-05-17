541e23aadb7ad31647f94e4a11b08f70bc48be4d
Quentin ADAM <quentin.adam@waxzce.org>
2012-05-17 22:54:22 +0200

    something in the app

--- openspice-spotifyapp/js/script.js
+++ openspice-spotifyapp/js/script.js
@@ -1,18 +1,36 @@
 var sp = getSpotifyApi(1);
 var models = sp.require('sp://import/scripts/api/models');
 
+var os_templates = {
+    trackInSearch: _.template('<tr><td><i class="icon-music"></i></td><td><%= name %></td><td><%= artists %></td><td><%= album%></td><td><button class="btn fnct_plus <%= disabled%>"><i class="icon-plus"></i></button></td></tr>'),
+
+    trackInAlbum: _.template('<tr><td><i class="icon-music"></i></td><td><%= number %></td><td><%= name %></td><td><%= artists %></td><td><button class="btn fnct_plus <%= disabled%>"><i class="icon-plus"></i></button></td></tr>'),
+
+    album: _.template('<tr><td><i class="icon-book"></i></td><td><%= name %></td><td><%= year %></td></tr>'),
+
+    currentlyPlaying: _.template('<h5><%= name %></h5><p><%= artists %></p>'),
+
+    trackInQueue: _.template('<li class="playlist_fellows"><i class="icon-music"></i><strong><%= name %></strong> - <%= artists %></li>')
+};
+
 
 $(function() {
     var socket = io.connect('http://colabzik.fake:8066/controll');
     socket.on('connect',
     function() {
-		socket.emit('country', models.session.country);
+        socket.emit('country', models.session.country);
 
         socket.on('playmusic_order',
         function(t) {
             models.player.play(t.href);
-			socket.emit('playing', t);
-			document.write('.');
+            socket.emit('playing', t);
+		        if (!_.isEmpty(t)) {
+		            $('#playing').html(os_templates.currentlyPlaying({
+		                name: t.name,
+		                artists: _.pluck(t.artists, 'name').join(', ')
+		            }));
+		        }
+			
         });
 
 
