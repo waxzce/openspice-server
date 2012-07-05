47313bdedbbc146f8e47a2980eb65cba1129a6b0
Quentin ADAM <quentin.adam@waxzce.org>
2012-07-05 08:56:45 +0200

    bugfix

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ function(socket) {
     function(data) {
         if(data.pass == masterpass){
             musicqueue.flushQueue();
+            clearTimeout(dospotify.timeoutId);
             io.sockets.emit('re_init');
         }
     });
